e2e0aa5aa93b61d07f66ee1fadd869ff5de14781
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2024-07-07 15:28:23 +0000

    go: changes for v0.0.50

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: dep test ncommit pull
 
-VERSION = v0.0.49
+VERSION = v0.0.50
 
 init:
 	go mod init github.com/nelsonsaake/go
--- do/die.go
+++ do/die.go
@@ -4,17 +4,15 @@ package do
 func Die(err any) {
 
 	switch err := err.(type) {
-	case error:
-		if err != nil {
-			panic(err)
-		}
 	case bool:
 		if !err {
 			panic("something went wrong")
 		}
 	case string:
 		panic(err)
-	default:
-		panic("something went wrong")
+	}
+
+	if err != nil {
+		panic(err)
 	}
 }
