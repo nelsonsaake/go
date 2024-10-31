c02c12969418aa18601fbadc2841023deb5f3137
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2024-10-31 08:38:56 +0000

    go: changes for v0.0.125

--- dirs/is_child.go
+++ dirs/is_child.go
@@ -1,10 +1,8 @@
 package dirs
 
-import (
-	"path/filepath"
-)
+import "strings"
 
 // IsChild: check to see if path, is a direct child of this dir
 func (dir *Dir) IsChild(path string) bool {
-	return clean(dir.path) == clean(filepath.Dir(path))
+	return strings.HasPrefix(clean(path), clean(dir.path))
 }
