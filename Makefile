64702e4c6c72737f6870abed0f8ff871d91fe228
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2024-10-31 08:51:41 +0000

    go: changes for v0.0.129

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: dep test ncommit pull dd
 
-VERSION = v0.0.128
+VERSION = v0.0.129
 
 init:
 	go mod init github.com/nelsonsaake/go
--- dirs/is_child.go
+++ dirs/is_child.go
@@ -1,9 +1,22 @@
 package dirs
 
-import "strings"
+import (
+	"path/filepath"
+	"strings"
+)
+
+func isParentDir(parentDir, childDir string) bool {
+	parentDir = filepath.Clean(parentDir)
+	childDir = filepath.Clean(childDir)
+
+	// Normalize the paths to ensure consistent comparison
+	parentDir = filepath.ToSlash(parentDir)
+	childDir = filepath.ToSlash(childDir)
+
+	return strings.HasPrefix(childDir, parentDir+"/")
+}
 
 // IsChild: check to see if path, is a direct child of this dir
 func (dir *Dir) IsChild(path string) bool {
-	child, parent := clean(path), clean(dir.path)
-	return strings.HasPrefix(child, parent) && child != parent
+	return isParentDir(dir.path, path)
 }
