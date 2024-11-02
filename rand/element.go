412f6b38e3d32fde185fa22543079f5191cd1525
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2024-11-02 20:42:05 +0000

    go: changes for v0.0.132

---
+++ rand/element.go
@@ -0,0 +1,19 @@
+package rand
+
+import "math/rand"
+
+func RandElement[T any](ls ...T) T {
+
+	if len(ls) == 0 {
+		panic("cannot pick a random element from an empty list")
+	}
+
+	var (
+		min = 0
+		max = len(ls) - 1
+	)
+
+	i := rand.Intn(max-min+1) + min
+
+	return ls[i]
+}
