af558f907a010b6faabc425f822264c0f3a573e0
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-03-13 15:48:45 +0000

    go: changes for v0.0.145

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: dep test ncommit pull dd
 
-VERSION = v0.0.144
+VERSION = v0.0.145
 
 init:
 	go mod init github.com/nelsonsaake/go
--- rand/int.go
+++ rand/int.go
@@ -2,15 +2,21 @@ package rand
 
 import (
 	"math/rand"
+	"time"
 )
+
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func Int(max int) int {
 	if max == 0 {
 		return 0
 	}
-	return rand.Intn(max)
+	return rng.Intn(max)
 }
 
 func IntBetween(min, max int) int {
-	return rand.Intn(max-min+1) + min
+	if min >= max {
+		return min
+	}
+	return rng.Intn(max-min+1) + min
 }
