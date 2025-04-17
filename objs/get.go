c473ebd7dfc03753a3e4002d0e2b5dea6c5e573a
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-04-17 13:28:51 +0000

    go: changes for v0.0.166

--- objs/get.go
+++ objs/get.go
@@ -4,19 +4,34 @@ import "strings"
 
 func Get(m map[string]any, k string) any {
 
-	var res any = m
-
-	keys := strings.Split(k, ".")
-
-	for _, key := range keys {
-
-		_res, ok := res.(map[string]any)
-		if !ok {
-			return nil
-		}
-
-		res = _res[key]
+	if m == nil {
+		return nil
 	}
 
-	return res
+	if len(strings.TrimSpace(k)) == 0 {
+		return nil
+	}
+
+	v, ok := m[k]
+	if ok {
+		return v
+	}
+
+	ks := strings.Split(k, ".")
+	if len(ks) == 1 {
+		return nil
+	}
+
+	// key in the current object
+	k0 := ks[0]
+
+	// nested key
+	kk := strings.Join(ks[1:], ".")
+
+	mm, ok := m[k0].(map[string]any)
+	if !ok {
+		return nil
+	}
+
+	return Get(mm, kk)
 }
