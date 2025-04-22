3e7a7b80f8cf3467f263e8a9c4d9569273269fb7
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-04-22 21:01:58 +0000

    go: changes for v0.0.170

--- objs/get_map.go
+++ objs/get_map.go
@@ -8,6 +8,25 @@ func GetMap(m map[string]any, k string) map[string]any {
 	return v
 }
 
+func GetStringMap(m map[string]any, k string) map[string]string {
+
+	var (
+		from = Get(m, k)
+		to   = map[string]string{}
+	)
+
+	if from == nil {
+		return nil
+	}
+
+	err := cast(from, &to)
+	if err != nil {
+		return nil
+	}
+
+	return to
+}
+
 func GetSlice(m map[string]any, k string) []any {
 	v, ok := Get(m, k).([]any)
 	if !ok {
