687ab2a2f9d7d479302b7e5288b7c7abdf660e4a
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-05-10 10:48:01 +0000

    go: changes for v0.0.185

--- objs/obj.go
+++ objs/obj.go
@@ -25,7 +25,17 @@ func (m Obj) Get(key string) any {
 	return Get(m.Data, key)
 }
 
+// G: get any
+func (m Obj) G(key string) any {
+	return Get(m.Data, key)
+}
+
 func (m Obj) GetString(key string) string {
+	return GetString(m.Data, key)
+}
+
+// GS: get string
+func (m Obj) GS(key string) string {
 	return GetString(m.Data, key)
 }
 
@@ -33,7 +43,17 @@ func (m Obj) GetInt(key string) int {
 	return GetInt(m.Data, key)
 }
 
+// GI: get int
+func (m Obj) GI(key string) int {
+	return GetInt(m.Data, key)
+}
+
 func (m Obj) GetFloat64(key string) float64 {
+	return GetFloat64(m.Data, key)
+}
+
+// GF: get float64
+func (m Obj) GF(key string) float64 {
 	return GetFloat64(m.Data, key)
 }
 
@@ -41,7 +61,17 @@ func (m Obj) GetBool(key string) bool {
 	return GetBool(m.Data, key)
 }
 
+// GB: get bool
+func (m Obj) GB(key string) bool {
+	return GetBool(m.Data, key)
+}
+
 func (m Obj) GetMap(key string) map[string]any {
+	return GetMap(m.Data, key)
+}
+
+// GM: get map[string]any
+func (m Obj) GM(key string) map[string]any {
 	return GetMap(m.Data, key)
 }
 
@@ -49,7 +79,17 @@ func (m Obj) GetStringMap(key string) map[string]string {
 	return GetStringMap(m.Data, key)
 }
 
+// GSM: get string map
+func (m Obj) GSM(key string) map[string]string {
+	return GetStringMap(m.Data, key)
+}
+
 func (m Obj) GetSlice(key string) []any {
+	return GetSlice(m.Data, key)
+}
+
+// GSL: get slice []any
+func (m Obj) GSL(key string) []any {
 	return GetSlice(m.Data, key)
 }
 
@@ -57,7 +97,17 @@ func (m Obj) GetStringSlice(key string) []string {
 	return GetStringSlice(m.Data, key)
 }
 
+// GSS: get string slice
+func (m Obj) GSS(key string) []string {
+	return GetStringSlice(m.Data, key)
+}
+
 func (m Obj) GetObj(key string) *Obj {
+	return GetObj(m.Data, key)
+}
+
+// GO: get Obj
+func (m Obj) GO(key string) *Obj {
 	return GetObj(m.Data, key)
 }
 
