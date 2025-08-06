b33f000868afaab55abd8dd47f5bb2abe95aee7f
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-08-06 21:28:39 +0000

    go: changes for v0.0.211

--- app/rsc/logger.go
+++ app/rsc/logger.go
@@ -9,14 +9,16 @@ import (
 // SilentLogger disables all output from the default logger
 type SilentLogger struct{}
 
-func (SilentLogger) Setup() {
+func (SilentLogger) Setup() error {
 	log.SetOutput(io.Discard)
+	return nil
 }
 
 // VerboseLogger enables simple plain output (no timestamps, no prefixes)
 type VerboseLogger struct{}
 
-func (VerboseLogger) Setup() {
+func (VerboseLogger) Setup() error {
 	log.SetOutput(os.Stdout)
 	log.SetFlags(0) // no timestamp, no file info
+	return nil
 }
