8673205791038aad58b7fe49997b9dc6475a4a8e
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-08-16 06:39:34 +0000

    go: changes for v0.0.227

--- cfgs/cfgs.go
+++ cfgs/cfgs.go
@@ -1,33 +1,11 @@
 package cfgs
 
+import "github.com/nelsonsaake/go/objs"
+
 var defaultCfg *Config
 
 func Load(dirs ...string) {
-
 	defaultCfg = New(dirs...)
-
-	GetBool = defaultCfg.GetBool
-	GetFloat64 = defaultCfg.GetFloat64
-	GetInt = defaultCfg.GetInt
-	GetString = defaultCfg.GetString
-	GetStringMap = defaultCfg.GetStringMap
-	GetMap = defaultCfg.GetMap
-	GetSlice = defaultCfg.GetSlice
-	GetStringSlice = defaultCfg.GetStringSlice
-	GetObj = defaultCfg.GetObj
-	GetAs = defaultCfg.GetAs
-	Get = defaultCfg.Get
-
-	GS = GetString
-	GSI = GetStringSlice
-	GI = GetInt
-	GF = GetFloat64
-	GB = GetBool
-	GM = GetMap
-	GSM = GetStringMap
-	GSL = GetSlice
-	GO = GetObj
-	GA = Get
 }
 
 type cfgsSetup struct {
@@ -43,19 +21,52 @@ func Setup(dirs ...string) cfgsSetup {
 	return cfgsSetup{dirs: dirs}
 }
 
-var (
-	GetBool        = defaultCfg.GetBool
-	GetFloat64     = defaultCfg.GetFloat64
-	GetInt         = defaultCfg.GetInt
-	GetString      = defaultCfg.GetString
-	GetStringMap   = defaultCfg.GetStringMap
-	GetMap         = defaultCfg.GetMap
-	GetSlice       = defaultCfg.GetSlice
-	GetStringSlice = defaultCfg.GetStringSlice
-	GetObj         = defaultCfg.GetObj
-	GetAs          = defaultCfg.GetAs
-	Get            = defaultCfg.Get
+func Get(key ...string) any {
+	return defaultCfg.Get(key...)
+}
 
+func GetString(key ...string) string {
+	return defaultCfg.GetString(key...)
+}
+
+func GetInt(key ...string) int {
+	return defaultCfg.GetInt(key...)
+}
+
+func GetBool(key ...string) bool {
+	return defaultCfg.GetBool(key...)
+}
+
+func GetFloat64(key ...string) float64 {
+	return defaultCfg.GetFloat64(key...)
+}
+
+func GetMap(key ...string) map[string]any {
+	return defaultCfg.GetMap(key...)
+}
+
+func GetStringMap(key ...string) map[string]string {
+	return defaultCfg.GetStringMap(key...)
+}
+
+func GetSlice(key ...string) []any {
+	return defaultCfg.GetSlice(key...)
+}
+
+func GetStringSlice(key ...string) []string {
+	return defaultCfg.GetStringSlice(key...)
+}
+
+func GetObj(key ...string) *objs.Obj {
+	return defaultCfg.GetObj(key...)
+}
+
+func GetAs(dest any, key ...string) {
+	defaultCfg.GetAs(dest, key...)
+}
+
+// Aliases
+var (
 	GS  = GetString
 	GSI = GetStringSlice
 	GI  = GetInt
