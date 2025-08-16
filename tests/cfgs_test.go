52ce3ab67f30fa59b2a756d9e587b66f0ba75d82
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-08-16 01:59:23 +0000

    go: changes for v0.0.219

--- tests/cfgs_test.go
+++ tests/cfgs_test.go
@@ -1,10 +1,12 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/nelsonsaake/go/afs"
 	"github.com/nelsonsaake/go/cfgs"
+	"github.com/nelsonsaake/go/pty"
 )
 
 func TestCfgs(t *testing.T) {
@@ -12,9 +14,35 @@ func TestCfgs(t *testing.T) {
 		afs.Path("src/configs"),
 	)
 
+	ln := func(arg ...int) {
+		ln := ""
+
+		var count = 0
+
+		if len(arg) == 0 {
+			count = 2
+		}
+
+		for i := range arg {
+			count += i
+		}
+
+		for range count {
+			ln += "\n"
+		}
+
+		fmt.Print(ln)
+	}
+
 	t.Log("dbs.model")
-	t.Log(c.Get("dbs.model"))
+	ln(1)
+
+	t.Log(pty.JSON(c.Get("dbs.model")))
+	ln()
 
 	t.Log("dbs.model.excluded")
-	t.Log(c.Get("dbs.model.excluded"))
+	ln(1)
+
+	t.Log(pty.JSON(c.Get("dbs.model.excluded")))
+	ln()
 }
