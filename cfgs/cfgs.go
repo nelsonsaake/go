e3d0dc94d284c61600cdb1fc28c3aad1f1d42009
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-08-16 06:35:47 +0000

    go: changes for v0.0.226

--- cfgs/cfgs.go
+++ cfgs/cfgs.go
@@ -3,7 +3,31 @@ package cfgs
 var defaultCfg *Config
 
 func Load(dirs ...string) {
+
 	defaultCfg = New(dirs...)
+
+	GetBool = defaultCfg.GetBool
+	GetFloat64 = defaultCfg.GetFloat64
+	GetInt = defaultCfg.GetInt
+	GetString = defaultCfg.GetString
+	GetStringMap = defaultCfg.GetStringMap
+	GetMap = defaultCfg.GetMap
+	GetSlice = defaultCfg.GetSlice
+	GetStringSlice = defaultCfg.GetStringSlice
+	GetObj = defaultCfg.GetObj
+	GetAs = defaultCfg.GetAs
+	Get = defaultCfg.Get
+
+	GS = GetString
+	GSI = GetStringSlice
+	GI = GetInt
+	GF = GetFloat64
+	GB = GetBool
+	GM = GetMap
+	GSM = GetStringMap
+	GSL = GetSlice
+	GO = GetObj
+	GA = Get
 }
 
 type cfgsSetup struct {
