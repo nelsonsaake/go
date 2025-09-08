1e981c0216d36a7c7e5d00ae5e91ef045a8f7283
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-09-08 20:40:05 +0000

    go: changes for v0.0.245

--- dsns/from_env.go
+++ dsns/from_env.go
@@ -10,6 +10,6 @@ func FromEnv() string {
 		WithPort(os.Getenv("DB_PORT")),
 		WithUsername(os.Getenv("DB_USERNAME")),
 		WithPassword(os.Getenv("DB_PASSWORD")),
-		WithDB(os.Getenv("DB_NAME")),
+		WithDB(os.Getenv("DB_DATABASE")),
 	)
 }
