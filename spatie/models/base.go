943e3f5aa372d50f8ce9a18613f9e222b0263873
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-09-09 06:16:54 +0000

    go: changes for v0.0.249

--- spatie/models/base.go
+++ spatie/models/base.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Base struct {
-	// ID        string    `gorm:"primaryKey;type:char(36);not null"`
-	ID        string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	// ID        string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	ID        string    `gorm:"primaryKey;type:char(36);not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
