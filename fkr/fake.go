81ea51b135fe0e519d57f74a75d36571a19ee634
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-09-11 21:04:41 +0000

    go: changes for v0.0.254

--- fkr/fake.go
+++ fkr/fake.go
@@ -4,10 +4,16 @@ import (
 	"math/rand/v2"
 
 	"github.com/brianvoe/gofakeit/v6"
+	"github.com/nelsonsaake/go/strs"
 )
 
 func init() {
 	gofakeit.Seed(0)
+}
+
+// todo: break the dependency on strs package
+func UUID() string {
+	return strs.UUID()
 }
 
 func Word() string {
@@ -20,4 +26,12 @@ func Name() string {
 
 func Bool() bool {
 	return rand.Float32() >= 0.5
+}
+
+func Sentence() string {
+	return gofakeit.Sentence(10)
+}
+
+func IPv4() string {
+	return gofakeit.IPv4Address()
 }
