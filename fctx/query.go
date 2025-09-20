360779f116cd5520acbd0c507cfb1ade0877d446
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-09-20 20:46:32 +0000

    go: changes for v0.0.294

--- fctx/query.go
+++ fctx/query.go
@@ -1,19 +1,35 @@
 package fctx
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type Query struct {
-	Search    string   `query:"search"`
-	With      []string `query:"with"`
-	Pagiation bool     `query:"pagination"`
-	Page      int      `query:"page"`
-	PageSize  int      `query:"pageSize"`
+	Search      string   `query:"search"`
+	With        []string `query:"with"`
+	NoPagiation bool     `query:"no_pagination"`
+	Page        int      `query:"page"`
+	PageSize    int      `query:"pageSize"`
 }
 
-func GetQuery(c *fiber.Ctx) *Query {
+func GetQuery(c *fiber.Ctx) (*Query, error) {
 
 	var q Query
-	c.QueryParser(q)
 
-	return &q
+	err := c.QueryParser(&q)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse query params: %w", err)
+	}
+
+	if q.Page == 0 {
+		q.Page = 1
+	}
+
+	if q.PageSize == 0 {
+		q.PageSize = 20
+	}
+
+	return &q, nil
 }
