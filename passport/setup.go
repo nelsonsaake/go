921ff266f3ceb3cdb6d59e647e22399e7d52e88b
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-09-20 12:11:52 +0000

    go: changes for v0.0.277

--- passport/setup.go
+++ passport/setup.go
@@ -2,6 +2,7 @@ package passport
 
 import (
 	"context"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -17,5 +18,31 @@ type Config struct {
 }
 
 func Setup(cfg Config) {
+
 	cfgInstance = &cfg
+
+	if cfg.TokenConfigs != nil {
+		return
+	}
+
+	cfg.TokenConfigs = map[string]TokenConfig{
+		"access": {
+			Name:          "access",
+			TTL:           time.Minute * 5, // 5 minutes
+			SigningKey:    "a6823c08-65de-4599-8698-64e78da6e2db",
+			SigningMethod: "HS256",
+		},
+		"refresh": {
+			Name:          "refresh",
+			TTL:           time.Minute * 60 * 24 * 30 * 6, // six months
+			SigningKey:    "4c0b1f88-ea83-46c0-8ce1-7da3a2cf5096",
+			SigningMethod: "HS256",
+		},
+		"reset": {
+			Name:          "reset",
+			TTL:           time.Minute * 30, // 30 minutes
+			SigningKey:    "f5884751-dfd0-4054-99de-af33162e7075",
+			SigningMethod: "HS256",
+		},
+	}
 }
