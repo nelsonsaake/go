abf0d8e09e62ed406e9186b6c85f028f22665f06
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-09-20 21:08:37 +0000

    go: changes for v0.0.297

---
+++ fctx/get.go
@@ -0,0 +1,47 @@
+package fctx
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type GetRequest struct {
+	Search       string   `query:"search" json:"search"`
+	With         []string `query:"with" json:"with"`
+	NoPagination bool     `query:"no_pagination" json:"no_pagination"`
+	Page         int      `query:"page" json:"page"`
+	PageSize     int      `query:"pageSize" json:"pageSize"`
+}
+
+var zeroQuery = GetRequest{
+	Page:     1,
+	PageSize: 20,
+}
+
+func GetQuery(c *fiber.Ctx) (GetRequest, error) {
+
+	var q = zeroQuery
+
+	err := c.QueryParser(&q)
+	if err != nil {
+		return zeroQuery, fmt.Errorf("failed to parse query params: %w", err)
+	}
+
+	if q.Page == 0 {
+		q.Page = 1
+	}
+
+	if q.PageSize == 0 {
+		q.PageSize = 20
+	}
+
+	return q, nil
+}
+
+//
+
+type GetResponse[T any] struct {
+	GetRequest
+	Data []T `json:"data"`
+}
