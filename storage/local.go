acba8b18f73550d5c67f36a83c424d1eec3868bc
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-09-28 13:58:21 +0000

    go: changes for v0.0.327

--- storage/local.go
+++ storage/local.go
@@ -1,9 +1,12 @@
 package storage
 
-import "github.com/nelsonsaake/go/storage/local"
+import (
+	"github.com/nelsonsaake/go/afs"
+	"github.com/nelsonsaake/go/storage/local"
+)
 
 func Local() Storage {
-	return local.New("/storage")
+	return local.New(afs.Path("/storage"))
 }
 
 func Store(file string) (string, error) {
