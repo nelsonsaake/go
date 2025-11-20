6c77cd8fbe8f518a45827e174ca268d01d796725
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-11-20 20:09:09 +0000

    patch: routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,20 +2,18 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-var registry *fiber.App
+type registerfunc func(*fiber.App)
 
-func init() {
-	registry = fiber.New()
-}
+var rfs = []registerfunc{}
 
 func Register(r func(*fiber.App)) {
-	r(registry)
+	rfs = append(rfs, r)
 }
 
 func Load(app *fiber.App) {
-	app.Use(logger.New())
-	app.Mount("/", registry)
+	for _, f := range rfs {
+		f(app)
+	}
 }
