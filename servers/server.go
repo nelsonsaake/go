2f6d19d1cfefd564b7c6612265cd1ea8a54b4614
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-11-26 04:24:23 +0000

    go: changes for v0.0.372

--- servers/server.go
+++ servers/server.go
@@ -1,27 +1,5 @@
 package servers
 
-import (
-	"context"
-	"fmt"
-
-	"github.com/gofiber/fiber/v2"
-	"golang.ngrok.com/ngrok"
-	"golang.ngrok.com/ngrok/config"
-)
-
-type NgrokServer struct {
-	App *fiber.App
-}
-
-func (s *NgrokServer) Run() error {
-	tun, err := ngrok.Listen(context.Background(), config.HTTPEndpoint(), ngrok.WithAuthtokenFromEnv())
-	if err != nil {
-		return fmt.Errorf("error opening an ngrok tunnel %v", err)
-	}
-	return s.App.Listener(tun)
-}
-
-func NewNgrokServer() *NgrokServer {
-	app := fiber.New()
-	return &NgrokServer{App: app}
+type Server interface {
+	Run() error
 }
