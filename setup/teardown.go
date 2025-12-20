e177ded7ca52f5efc899f1446c456e3fad75b851
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2025-12-20 20:25:56 +0000

    go: changes for v0.0.399

---
+++ setup/teardown.go
@@ -0,0 +1,30 @@
+package setup
+
+import (
+	"github.com/nelsonsaake/go/kvs"
+	"github.com/nelsonsaake/go/kvs/meta"
+	"github.com/nelsonsaake/go/settings"
+)
+
+func (b *BaseBuilder) Teardown() error {
+
+	if b.KvsEnabled {
+		if err := kvs.Teardown(); err != nil {
+			return err
+		}
+	}
+
+	if b.SettingsEnabled {
+		if err := settings.Teardown(); err != nil {
+			return err
+		}
+	}
+
+	if b.MetaEnabled {
+		if err := meta.Teardown(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
