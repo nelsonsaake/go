6839d1347d165791491ba4b8b7277bb100df0481
Nelson Kofi Saake <nelsonsaakekofi@gmail.com>
2026-04-11 13:56:31 +0000

    feat: ufs.SymlinkExists

--- ufs/exists.go
+++ ufs/exists.go
@@ -2,9 +2,20 @@ package ufs
 
 import "os"
 
-// Exists returns whether the given file or directory Exists
+// Exists returns whether the given file or directory exists
 func Exists(path string) (bool, error) {
 	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func SymlinkExists(path string) (bool, error) {
+	_, err := os.Lstat(path)
 	if err == nil {
 		return true, nil
 	}
